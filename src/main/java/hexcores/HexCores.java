48f6895727cf1a2b121925908734adc417bceb64
dream <709806504@qq.com>
2018-03-07 10:26:41 +0800

    fix code

--- src/main/java/hexcores/HexCores.java
+++ src/main/java/hexcores/HexCores.java
@@ -2,23 +2,29 @@ package hexcores;
 
 import basemod.BaseMod;
 import basemod.ModPanel;
+import basemod.interfaces.EditRelicsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
 import basemod.interfaces.PostInitializeSubscriber;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+
 import hexcores.relics.AmplifyCore;
 import hexcores.relics.BulkCore;
 import hexcores.relics.TransformerCore;
 
 import java.nio.charset.StandardCharsets;
 
-public class HexCores implements PostInitializeSubscriber {
+public class HexCores implements PostInitializeSubscriber,EditStringsSubscriber,EditRelicsSubscriber {
     private static final String MODNAME = "Hex Cores";
     private static final String AUTHOR = "cariehl";
     private static final String DESCRIPTION = "v1.0.0 NL Adds three new relics inspired by /u/Sweet_Azu.";
 
     public HexCores() {
         BaseMod.subscribeToPostInitialize(this);
+        BaseMod.subscribeToEditRelics(this);
+        BaseMod.subscribeToEditStrings(this);
     }
 
     public static void initialize() {
@@ -31,14 +37,20 @@ public class HexCores implements PostInitializeSubscriber {
         ModPanel settingsPanel = new ModPanel();
         settingsPanel.addLabel("This mod does not have any settings", 400.0f, 700.0f, (me) -> {});
         BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
+    }
 
-        // RelicStrings
-        String jsonString = Gdx.files.internal("localization/HexCores-relics.json").readString(String.valueOf(StandardCharsets.UTF_8));
-        BaseMod.loadCustomRelicStrings(jsonString);
-
+    @Override
+    public void receiveEditRelics() {
         // Add relics
         RelicLibrary.add(new AmplifyCore());
         RelicLibrary.add(new BulkCore());
         RelicLibrary.add(new TransformerCore());
+    }
+
+    @Override
+    public void receiveEditStrings() {
+        // RelicStrings
+        String jsonString = Gdx.files.internal("localization/HexCores-relics.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        BaseMod.loadCustomStrings(RelicStrings.class, jsonString);
     }
 }
